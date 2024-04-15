61f0aba5c028e96a260d7212ebca31a98df74e86
Erilea <erilea@erilea.fr>
2024-04-15 15:59:28 +0200

    Plug UI and permalink calculations with the settings randomizer

--- randomizer.py
+++ randomizer.py
@@ -508,6 +508,8 @@ class WWRandomizer:
     for option in Options.all:
       if not option.permalink:
         continue
+      if options.randomize_settings and SettingsRandomizer.weights("default").is_managed(option):
+        continue
       
       value = options[option.name]
       
@@ -583,6 +585,8 @@ class WWRandomizer:
     bitsreader = PackedBitsReader(option_bytes)
     for option in Options.all:
       if not option.permalink:
+        continue
+      if options.randomize_settings and SettingsRandomizer.weights("default").is_managed(option):
         continue
       
       if issubclass(option.type, bool):
