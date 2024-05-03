81aae4858ab4da7c357fe399080bb02fba40d4b1
Erilea <erilea@erilea.fr>
2024-05-03 15:58:34 +0200

    Add Season Roulette Experimental Settings
    Choose one season of racing settings, at random. Used to showcase
    randomly selecting spoiler logs

--- options/randomized/data.py
+++ options/randomized/data.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 
 import yaml
@@ -6,6 +7,15 @@ from wwrando_paths import DATA_PATH
 
 WEIGHTS_PATH = os.path.join(DATA_PATH, "random_settings_weights.yml")
 
+
+def flatten(loader: yaml.Loader | yaml.FullLoader | yaml.SafeLoader, node):
+    """Flattens one layer of a list of T|list[T]"""
+    parsed_node = loader.construct_sequence(node)
+    return list(itertools.chain.from_iterable(
+        (item,) if not isinstance(item, list) else item
+        for item in parsed_node
+    ))
+yaml.add_constructor('!flatten', flatten, yaml.FullLoader)
 
 def load_data_files(file=WEIGHTS_PATH):
     with open(file) as f:
