0becf0b16006b8b9a0fc38dc1698cded5e0b7f88
Erilea <erilea@erilea.fr>
2024-06-27 13:53:59 +0200

    Skip loading enemy randomizer yaml files when disabled
    Loading the enemy randomizer yaml files accounts for almost 20% of
    randomization time, even when the enemy randomizer is completely
    disabled, because the enemy data files are huge

--- randomizers/enemies.py
+++ randomizers/enemies.py
@@ -143,6 +143,9 @@ class EnemyRandomizer(BaseRandomizer):
   def __init__(self, rando):
     super().__init__(rando)
     
+    if not self.is_enabled():
+      # Loading all the yaml files takes a surprising amount of time, skip it if we're not going to use them
+      return
     with open(os.path.join(DATA_PATH, "enemy_types.txt"), "r") as f:
       self.enemy_types = yaml.load(f)
     
